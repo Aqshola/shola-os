1cde43a6a658cb3ffd116748cac109f21ed6ce3a
aqshola <aqshola.afid@gmail.com>
2026-02-22 20:18:56 +0700

    feat: add desktop application icons and layout

---
+++ src/apps/desktop-icon-app.ts
@@ -0,0 +1,12 @@
+export const LIST_DESKTOP_APP = [{
+    title: "Resume",
+    alt: "Resume",
+    icon: "/assets/icons/certificate_2.ico",
+},
+{
+    title: "Portofolio",
+    alt: "Portofolio",
+    icon: "/assets/icons/kodak_imaging.ico",
+},
+
+]
--- src/pages/Desktop/components/main.tsx
+++ src/pages/Desktop/components/main.tsx
@@ -1,7 +1,15 @@
+import { LIST_DESKTOP_APP } from '../../../apps/desktop-icon-app'
 import '../style/window.css'
 export default function Window() {
     return (
-    <div class="desktop">
-        
-    </div>)
+        <div class="desktop">
+            <div class='container-desktop-icon'>
+                {LIST_DESKTOP_APP.map((app) => (
+                <div class='desktop-icon'>
+                    <img src={app.icon} alt={app.alt} class="desktop-icon-image" />
+                    <p class='desktop-icon-text'>{app.title}</p>
+                </div>
+                ))}
+            </div>
+        </div>)
 }
