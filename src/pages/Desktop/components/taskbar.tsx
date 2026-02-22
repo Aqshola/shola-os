0f9c39652ac6b09c043022ac4e77ba3e5c17f496
aqshola <aqshola.afid@gmail.com>
2026-02-22 22:26:45 +0700

    feat: implement start menu with items and styling

--- src/pages/Desktop/components/taskbar.tsx
+++ src/pages/Desktop/components/taskbar.tsx
@@ -4,23 +4,23 @@ import "../style/taskbar.css"
 export default function Taskbar() {
     const [time, setTime] = createSignal(new Date());
 
-  // Equivalent to useEffect(..., [])
-  const timer = setInterval(() => setTime(new Date()), 1000);
+    // Equivalent to useEffect(..., [])
+    const timer = setInterval(() => setTime(new Date()), 1000);
 
-  // Cleanup to prevent memory leaks
-  onCleanup(() => clearInterval(timer));
+    // Cleanup to prevent memory leaks
+    onCleanup(() => clearInterval(timer));
 
-  const formatTime = (date:Date) => {
-    let hours = date.getHours();
-    let minutes:string|number = date.getMinutes();
-    const ampm = hours >= 12 ? 'PM' : 'AM';
-    
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    
-    return `${hours}:${minutes} ${ampm}`;
-  };
+    const formatTime = (date: Date) => {
+        let hours = date.getHours();
+        let minutes: string | number = date.getMinutes();
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+
+        return `${hours}:${minutes} ${ampm}`;
+    };
     return (
         <div class="taskbar">
             <div>
@@ -32,6 +32,38 @@ export default function Taskbar() {
                         Start
                     </span>
                 </button>
+                <div class="window start-menu" >
+                    <div class="start-menu-sidebar">
+                        <span>Aqshol OS</span>
+                    </div>
+                    <div class="start-menu-items">
+                       <button
+                        class="start-menu-item"
+                    >
+                        
+                       <span>Portofolio</span>
+                    </button>
+
+                    <button
+                        class="start-menu-item"
+                    >
+                        
+                       <span>Resume</span>
+                    </button>
+                    <button
+                        class="start-menu-item"
+                    >
+                        
+                       <span>Github</span>
+                    </button>
+                    <button
+                        class="start-menu-item"
+                    >
+                        
+                       <span>LInkedin</span>
+                    </button>
+                    </div>
+                </div>
             </div>
 
 
