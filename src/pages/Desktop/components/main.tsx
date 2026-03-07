4eaaffb02243c0f24f0354e47cce3c7dacf7522a
Aqshol Claw <aqshol@example.com>
2026-03-07 04:10:22 +0000

    Add desktop icon click/double-click to open apps

--- src/pages/Desktop/components/main.tsx
+++ src/pages/Desktop/components/main.tsx
@@ -1,17 +1,50 @@
+import { createSignal, onMount } from 'solid-js';
 import '@/pages/Desktop/style/window.css'
-import { windowStore } from '@/stores/windowStore'
+import { windowStore, addActiveWindow, registerWindow } from '@/stores/windowStore'
+import { useDeviceType } from '@/hooks/useDeviceType';
+
 export default function Window() {
-    const desktopApp=windowStore.appList.filter(app=>app.showIn.desktop)
+    const deviceType = useDeviceType();
+    const desktopApp = windowStore.appList.filter(app => app.showIn?.desktop);
+    
+    const handleClick = (app: any) => {
+        if (deviceType() === "mobile") {
+            openApp(app);
+        }
+    };
+    
+    const handleDoubleClick = (app: any) => {
+        if (deviceType() === "desktop") {
+            openApp(app);
+        }
+    };
+    
+    const openApp = (app: any) => {
+        if (app.type === "window") {
+            registerWindow(app.id);
+            addActiveWindow(app);
+            app.action();
+        } else if (app.type === "external") {
+            app.action();
+        } else if (app.type === "action") {
+            app.action();
+        }
+    };
+
     return (
         <div class="desktop">
             <div class='container-desktop-icon'>
                 {desktopApp.map((app) => (
-                <div class='desktop-icon'>
+                <div 
+                    class='desktop-icon'
+                    onClick={() => handleClick(app)}
+                    onDblClick={() => handleDoubleClick(app)}
+                >
                     <img src={app.icon} alt={app.title} class="desktop-icon-image" />
                     <p class='desktop-icon-text'>{app.title}</p>
                 </div>
                 ))}
             </div>
         </div>
-        )
+    )
 }
